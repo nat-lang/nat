b5604c255c2c42017e6eb751a996eb0181c02a32
Alexander Shilen <aps2163@columbia.edu>
2022-09-12 14:24:58 -0400

    wip infinite types, domains, and ty ops

--- src/Mean/Dom.hs
+++ src/Mean/Dom.hs
@@ -1,1 +1,10 @@
+{-# LANGUAGE GADTs #-}
+
 module Mean.Dom where
+
+import Mean.Core
+import Mean.Set
+import Mean.Syntax
+
+data DomExpr a where
+  Dom :: Expressible a => Type -> Set a -> DomExpr a
